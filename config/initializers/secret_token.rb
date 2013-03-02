2d2e0bea16348e835a1dca8f9232d570966a546a
Siddharth Batra <siddharthabatra@gmail.com>
2013-03-01 16:12:36 -0800

    start of word games.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordgames::Application.config.secret_token = '5c2a13bb68e425a9443cbfb35b88be7b04221a6ae47be7f1e56395852a0054d269392ca0de7ffb0ce791b246d080a3f4f59904c9da83fd63c43434c75aad8933'
