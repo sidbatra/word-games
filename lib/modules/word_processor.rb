a0a0a3c53364c9bd0aed9ed371e66e1a4358fd66
Siddharth Batra <siddharthabatra@gmail.com>
2013-03-03 13:41:07 -0800

    anagam method in word processor

--- lib/modules/word_processor.rb
+++ lib/modules/word_processor.rb
@@ -7,4 +7,19 @@ module WordProcessor
     Word.where(sorted_text: scrambled_word.split(//).sort.join).pluck(:text)
   end
 
+  # Generate anagrams of length 3 and above from the given word.
+  #
+  def self.anagram(word)
+    words = []
+    chars = word.split(//)
+
+    (3..chars.length).each do |length|
+      chars.combination(length).each do |combination|
+        words << combination.sort.join
+      end
+    end
+
+    Word.where(sorted_text: words).pluck(:text).sort{|a,b| a.length <=> b.length}
+  end
+
 end
