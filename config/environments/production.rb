d1ef9535df8cbee0c9c88055abd8c2d9e5348db9
Siddharth Batra <siddharthabatra@gmail.com>
2013-08-03 14:59:43 -0700

    correcting typos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-WordGames::Application.configure do
+Wordgames::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
